f6feebc1d262ce48ead8b73d975d268b8166185f
Komissko <kkomisarov@gmail.com>
2018-03-06 19:23:29 +0300

    Starting family pages

--- test/controllers/my_family_controller_test.rb
+++ test/controllers/my_family_controller_test.rb
@@ -4,22 +4,22 @@ class MyFamilyControllerTest < ActionController::TestCase
 
   def setup
     @base_title = " | LoveJoyCookies"
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Home#{@base_title}"
   end
+    test "should get home" do
+      get :home
+      assert_response :success
+      assert_select "title", "Home#{@base_title}"
+    end
 
-  test "should get dima" do
-    get :dima
-    assert_response :success
-    assert_select "title", "Dima#{@base_title}"
-  end
+    test "should get dima" do
+      get :dima
+      assert_response :success
+      assert_select "title", "Dima#{@base_title}"
+    end
 
-test "should get leva" do
-	get :leva
-	assert_response :success
-	assert_select "title", "Leva#{@base_title}"
-end
-
+    test "should get leva" do
+    	get :leva
+    	assert_response :success
+    	assert_select "title", "Leva#{@base_title}"
+    end
 end
