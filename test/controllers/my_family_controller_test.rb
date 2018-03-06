9b5110418edae7b8deb5ad8fce57c681fc0e41c1
Komissko <kkomisarov@gmail.com>
2018-03-06 19:14:40 +0300

    Start Family pages

--- test/controllers/my_family_controller_test.rb
+++ test/controllers/my_family_controller_test.rb
@@ -1,22 +1,25 @@
 require 'test_helper'
 
 class MyFamilyControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = " | LoveJoyCookies"
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | LoveJoyCookies"
+    assert_select "title", "Home#{@base_title}"
   end
 
   test "should get dima" do
     get :dima
     assert_response :success
-    assert_select "title", "Dima | LoveJoyCookies"
+    assert_select "title", "Dima#{@base_title}"
   end
 
 test "should get leva" do
 	get :leva
 	assert_response :success
-	assert_select "title", "Leva | LoveJoyCookies"
+	assert_select "title", "Leva#{@base_title}"
 end
 
 end
